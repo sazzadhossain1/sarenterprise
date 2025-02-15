40798fae7d266af1ec1b802de55f791d8de88c63
sazzad tomal <sazzadtomal1@gmail.com>
2025-02-15 15:07:29 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Main from "./LayOut/Main";
+import Home from "./components/Home/Home";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+          path: "/",
+          element: <Home></Home>,
+        },
+        {
+          path: "/home",
+          element: <Home></Home>,
+        },
+      ],
+    },
+  ]);
+  return (
+    <div className="App">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NavigationBar.css";
+
+const NavigationBar = () => {
+  return (
+    <div>
+      <h1>This is navigation</h1>
+    </div>
+  );
+};
+
+export default NavigationBar;
