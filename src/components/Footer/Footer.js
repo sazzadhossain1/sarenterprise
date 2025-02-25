8d2f7012656e70d34af577d866d884111b856584
sazzad tomal <sazzadtomal1@gmail.com>
2025-02-25 15:16:15 +0600

    add some info in footer's contact section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,10 +4,23 @@ const Footer = () => {
   return (
     <div className="footer_grid_div">
       <div>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Obcaecati earum totam quis ipsum?</p>
-        <p>Unde hic corporis aliquam consectetur!</p>
-        <p>Rerum saepe hic animi praesentium.</p>
+        <h1>Sar Enterprise</h1>
+        <p>
+          <b>Head Office :</b> 20/5, Wests Manikdi, Dhaka 1206.
+        </p>
+        <p>
+          <b>Brance Office:</b> House 28-30, West Avenue Road, Sector 16/B,
+          Uttara New Market, Uttara, Dhaka 1230
+        </p>
+        <p>
+          <b>Phone:</b> 01712-874402, 01716-532061, 0187-632259
+        </p>
+        <p>
+          <b>Email:</b> mssarenterprise2024@gmail.com
+        </p>
+        <p>
+          <b>Website:</b> www.sarenterprise.com
+        </p>
       </div>
       <div>
         <p>Lorem ipsum dolor sit amet.</p>
